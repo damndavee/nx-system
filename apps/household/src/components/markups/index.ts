b7ab3d823dcad09fef8ca1874197727920c0903e
Paweł Kopij <pawelrd@gmail.com>
2025-03-26 12:32:55 +0100

    add svg markups

---
+++ apps/household/src/components/markups/index.ts
@@ -0,0 +1,7 @@
+// https://undraw.co
+
+export { signinSvgMarkup } from './signin';
+export { signupSvgMarkup } from './signup';
+export { googleMarkup } from './google';
+export { facebookMarkup } from './facebook';
+export { appleMarkup } from './apple';
